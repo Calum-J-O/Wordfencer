afe4d533d7d58f3883b2c62051fc1c9c73d794eb
Calum <calum0797@gmail.com>
2020-03-09 22:24:58 +0000

    getting started

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,16 +7,18 @@ const {
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
-
+const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 
 function compileJs() {
     return src("src/**/*.js")
         .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(concat("bundle.js"))
+        .pipe(uglify())
+        .pipe(concat('sourcecode.js'))
+        .pipe(rename({ extname: '.min.js'}))
         .pipe(sourcemaps.write("."))
-        .pipe(dest('dist'));
+        .pipe(dest('build'));
 };
 
 function watchFiles() {
